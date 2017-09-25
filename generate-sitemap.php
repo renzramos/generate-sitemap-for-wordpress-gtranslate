31efcfd5e48d31e1877b44b3f58666809615234e
Renz Ramos <im.renzramos@gmail.com>
2017-09-25 09:28:33 +0800

    Create generate-sitemap.php

---
+++ generate-sitemap.php
@@ -0,0 +1,96 @@
+<?php
+/* 
+* Name: Generate Sitemap for GTranslate.io
+* Author: Renz Ramos
+* Version : 1.0
+*/
+
+define('WP_USE_THEMES', true);
+require( 'wp-load.php' );
+
+
+$data = get_option('GTranslate');
+$languages = $data['fincl_langs'];
+
+
+$output = '';
+
+$output.='<?xml version="1.0" encoding="UTF-8"?>'.PHP_EOL; 
+$output.='<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">' .PHP_EOL; 
+ 
+
+foreach ($languages as $language){
+    
+    if ($language == "en") continue;
+    
+    
+    // pages
+    $args = array(
+        'post_type' => array( 'page'),
+        'post_status' => array( 'publish'),
+        'posts_per_page' => -1
+    );
+    
+    $query = new WP_Query( $args );
+    
+    
+    if ( $query->have_posts() ) :
+        
+        while ( $query->have_posts() ) : $query->the_post(); 
+        
+        $url = str_replace(home_url('/'), home_url(). '/' . $language . '/', get_permalink());
+        $output.= urlElement($url);
+        
+        
+        endwhile; 
+        wp_reset_postdata();
+    endif; 
+    
+    // reviews
+    $args = array(
+        'post_type' => array( 'review'),
+        'post_status' => array( 'publish'),
+        'posts_per_page' => -1
+    );
+    
+    $query = new WP_Query( $args );
+    
+    
+    if ( $query->have_posts() ) :
+        
+        while ( $query->have_posts() ) : $query->the_post(); 
+        
+        $url = str_replace(home_url('/'), home_url(). '/' . $language . '/', get_permalink());
+        $output.= urlElement($url);
+        
+        
+        endwhile; 
+        wp_reset_postdata();
+    endif; 
+    
+    
+    //$output.= urlElement(home_url() . '/' . $language . '/');
+    
+    // check all pages
+    
+    
+    // check all reviews
+}
+
+
+$output.='</urlset>'; 
+$sitemap = fopen("generated-sitemap.xml", "w") or die("Unable to open file!");
+$write_status =  fwrite($sitemap, $output);
+fclose($sitemap);
+
+echo 'Sitemap Generated!';
+
+function urlElement($url) {
+    $url_output = '';
+    $url_output.='<url>'.PHP_EOL; 
+    $url_output.='<loc>'.$url.'</loc>'. PHP_EOL; 
+    $url_output.='<changefreq>weekly</changefreq>'.PHP_EOL; 
+    $url_output.='</url>'.PHP_EOL;
+    return $url_output;
+} 
+?>
